19312a23c4716ae3b132cf03e631632a8f98d865
Kenneth Espaldon <kenespaldon@gmail.com>
2025-04-11 16:07:38 -0700

    display player awards

--- scripts.js
+++ scripts.js
@@ -93,7 +93,7 @@ function toggleButtonState(e, nextCard) {
 function editCardContent(card, playerName, newImageURL) {
   card.style.display = "block";
 
-  const cardHeader = card.querySelector("h2");
+  const cardHeader = card.querySelector(".player-name");
   cardHeader.textContent = playerName;
 
   const cardImage = card.querySelector("img");
@@ -101,6 +101,7 @@ function editCardContent(card, playerName, newImageURL) {
   cardImage.alt = playerName;
 
   displayPlayerStats(card, playerName);
+  displayPlayerAwards(card, playerName);
 
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
@@ -151,6 +152,56 @@ function displayPlayerStats(card, name) {
   playoffsFgPct.textContent = playoffStats.fieldGoalPct;
   playoffsTpPct.textContent = playoffStats.threePointPct;
   playoffsFtPct.textContent = playoffStats.freeThrowPct;
+}
+
+function displayPlayerAwards(card, name) {
+  const playerAwards = players[name].awards;
+
+  const mvpAwards = card.querySelector(".mvp-awards");
+  const dpoyAwards = card.querySelector(".dpoy-awards");
+  const allNbaAwards = card.querySelector(".all-nba-awards");
+  const allDefAwards = card.querySelector(".all-defensive-awards");
+
+  const mvpCount = card.querySelector(".mvp-count");
+  const mvpYears = card.querySelector(".mvp-years");
+  const dpoyCount = card.querySelector(".dpoy-count");
+  const dpoyYears = card.querySelector(".dpoy-years");
+  const allNbaCount = card.querySelector(".all-nba-count");
+  const allNbaYears = card.querySelector(".all-nba-years");
+  const allDefCount = card.querySelector(".all-defensive-count");
+  const allDefYears = card.querySelector(".all-defensive-years");
+
+  const numberOfMVPs = playerAwards.mvp.yearsAwarded.length;
+  if (numberOfMVPs === 0) {
+    mvpAwards.classList.add("hidden");
+  } else {
+    mvpCount.textContent = `${numberOfMVPs}x Most Valuable Payer`;
+    mvpYears.textContent = `(${playerAwards.mvp.yearsAwarded.join(", ")})`;
+  }
+
+  const numberOfDPOYs = playerAwards.dpoy.yearsAwarded.length;
+  if (numberOfDPOYs === 0) {
+    dpoyAwards.classList.add("hidden");
+  } else {
+    dpoyCount.textContent = `${numberOfDPOYs}x Defensive Player of the Year`;
+    dpoyYears.textContent = `(${playerAwards.dpoy.yearsAwarded.join(", ")})`;
+  }
+
+  const numberOfAllNBASelections = playerAwards.allNBA.yearsAwarded.length;
+  if (numberOfAllNBASelections === 0) {
+    allNbaAwards.classList.add("hidden");
+  } else {
+    allNbaCount.textContent = `${numberOfAllNBASelections}x All-NBA`;
+    allNbaYears.textContent = `(${playerAwards.allNBA.yearsAwarded.join(", ")})`;
+  }
+
+  const numberOfAllDefSelections = playerAwards.allDefense.yearsAwarded.length;
+  if (numberOfAllDefSelections === 0) {
+    allDefAwards.classList.add("hidden");
+  } else {
+    allDefCount.textContent = `${numberOfAllDefSelections}x All-Defensive`;
+    allDefYears.textContent = `(${playerAwards.allDefense.yearsAwarded.join(", ")})`;
+  }
 }
 
 // This calls the addCards() function when the page is first loaded
@@ -224,10 +275,21 @@ function addPlayerStats(name) {
 function addPlayerAwards(name) {
   const player = players[name];
   const playerAwards = player.awards;
-  playerAwards.mvp.yearsAwarded = mvpInput.value.split(", ");
-  playerAwards.dpoy.yearsAwarded = dpoyInput.value.split(", ");
-  playerAwards.allNBA.yearsAwarded = allNbaInput.value.split(", ");
-  playerAwards.allDefense.yearsAwarded = allDefenseInput.value.split(", ");
+  if(mvpInput.value !== "") {
+    playerAwards.mvp.yearsAwarded = mvpInput.value.split(", ");
+  }
+
+  if(dpoyInput.value !== "") {
+    playerAwards.dpoy.yearsAwarded = dpoyInput.value.split(", ");
+  }
+
+  if(allNbaInput.value !== "") {
+    playerAwards.allNBA.yearsAwarded = allNbaInput.value.split(", ");
+  }
+
+  if(allDefenseInput.value !== "") {
+    playerAwards.allDefense.yearsAwarded = allDefenseInput.value.split(", ");
+  }
 }
 
 function addPlayerTeam(name) {
